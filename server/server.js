99fdcc05989ef74a61409d3b9e6f63c655338e11
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-07-21 19:13:21 +0530

    created routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-import express from "express"
-import cors from "cors"
-import 'dotenv/config'
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -12,15 +13,15 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }))
+app.use(cors({ credentials: true }));
 
 //Api Endpoints
 
-app.get('/', (req, res) => {
-    res.send("Api working")
-})
-
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
-    console.log(`Server started on port : ${port}`);
-})
+  console.log(`Server started on port : ${port}`);
+});
