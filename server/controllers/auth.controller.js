8f9a59ecd879b0ca18e311ac430b0bfb60abb866
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-07-25 10:45:39 +0530

    fixed return error and added verifyEmail function

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,44 +4,46 @@ import { User } from "../models/user.models.js"
 import transporter from "../config/nodemailer.js"
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body
-    if (!name || !email || !password) {
-        return res.json({success:false,message:"Missing Details"})
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing Details" });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
 
-    try {
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            return res.json({success:false,message:"User already exists"})
-        }
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const user = new User({ name, email, password: hashedPassword });
-        await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 1000,
+    });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge: 7 * 24 * 60 * 1000
-        })
+    //Sending welcome email
 
-        //Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to AuthService",
+      text: `Welcome to authService website . Your account has been created with email id: ${email}`,
+    };
 
-        const mailOptions = {
-            from: process.env.SENDER_EMAIL,
-            to: email,
-            subject: 'Welcome to AuthService',
-            text: `Welcome to authService website . Your account has been created with email id: ${email}`
-        };
+    await transporter.sendMail(mailOptions);
 
-        await transporter.sendMail(mailOptions);
-
-        return res.json({ success: true })
-    } catch (error) {
-      res.json({success:false,message: error.message})   
-    }
-}
+    return res.json({ success: true });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -70,7 +72,7 @@ export const login = async (req, res) => {
 
     return res.json({ success: true });
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message });
   }
 };
 
@@ -83,7 +85,7 @@ export const logout = async (req, res) => {
     });
     return res.json({ success: true, message: "Logged out" });
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message });
   }
 };
 
@@ -98,6 +100,33 @@ export const sendVerifyOtp = async (req, res) => {
       });
     }
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { userId, otp } = req.body;
+  if (!userId || !otp) {
+    return res.json({ success: false, message: "Missing Details" });
+  }
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    if (user.verifyOtp === "" || user.verifyOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP " });
+    }
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expired" });
+    }
+    user.isaccountVerified = true;
+    user.verifyOtp = "";
+    user.verifyOtpExpireAt = 0;
+
+    await user.save();
+    return res.json({ success: true, message: "Email Verified successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
 };
